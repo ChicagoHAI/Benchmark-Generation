63cd95d44ad9dc90aafc527632a93e309ff800bf
Your Name <you@example.com>
2025-06-02 10:49:53 -0500

    solving the context history

--- interactive_generation.py
+++ interactive_generation.py
@@ -5,6 +5,12 @@ headers = {
     "Content-Type": "application/json"
 }
 
+data = {
+    "model": "Qwen/Qwen2.5-7B-Instruct",
+    "messages": [
+    ]
+    }
+
 import sys
 
 class Tee:
@@ -25,27 +31,28 @@ class Tee:
 
 
 def chat (sentence):
-    data = {
-    "model": "Qwen/Qwen2.5-7B-Instruct",
-    "messages": [
-        {
+    user_message = {
             "role": "user",
             "content": sentence
         }
-    ]
-    }
+    data["messages"].append(user_message)
     response = requests.post(url, headers=headers, json=data)
     return response.json()
 
 
 def chat_message (sentence):
     response = chat(sentence)
+    assistant_message = {
+        "role": "assistant",
+        "content": response["choices"][0]['message']['content']
+        }
+    data["messages"].append(assistant_message)
     return response["choices"][0]['message']['content']
 
 
 def interactive (sentence):
     for counter in range(5):
-        print(chat_message(sentence + "first ask for details, don't generate directly"))
+        print(chat_message(sentence + " first ask for details, don't generate directly"))
         sentence = input("Input here:")
     print(chat_message(sentence + "Now, generate the benchmark."))
     return None
@@ -55,9 +62,5 @@ if __name__=="__main__":
     with open("output.txt", "a") as f:
         tee = Tee(sys.stdout, f)
         sys.stdout = tee
-        sentence = input("Please provide the detail of the benchmark you want to generate: ")
+        sentence = input(" Please provide the detail of the benchmark you want to generate: ")
         interactive(sentence)
-
-
-
-
